6d0a935ee57fa1a5d18a93a31b232ff5bb637ff1
Ray Healy <raymond.m.healy@gmail.com>
2026-03-03 20:27:02 -0500

    Util: Update documentation

--- lib/src/main/java/org/fairportrobotics/frc/robolib/Util.java
+++ lib/src/main/java/org/fairportrobotics/frc/robolib/Util.java
@@ -1,6 +1,23 @@
 package org.fairportrobotics.frc.robolib;
 
+/**
+ * Module defining general utility functions
+ */
 public abstract class Util {
+
+    /**
+     * <p> Get the mapping of value onto a range where rangeMin is 0, and
+     * rangeMax is 1 (i.e. what percent of the way does value lie between 
+     * rangeMin and rangeMax).</p>
+     * 
+     * <p> NOTE: rangeMin may be greater than rangeMax, but rangeMin must not
+     * equal rangeMax.</p>
+     *
+     * @param rangeMin The 0 point of the range
+     * @param rangeMax The 1 point of the range
+     * @param value The value to check
+     * @return The relative position of value in the range
+     */
     public static double rangePercent(double rangeMin, double rangeMax, double value) {
         double rangeWidth = rangeMax - rangeMin;
         double rangeOffset = value - rangeMin;
