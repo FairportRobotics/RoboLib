4a350ea49f2dfc51f62482cff8c32f11bfb8c676
TheRealGrayden <nmunier65@gmail.com>
2026-03-21 15:22:02 -0400

    Limelight

--- lib/src/main/java/org/fairportrobotics/frc/robolib/vision/limelight/Limelight.java
+++ lib/src/main/java/org/fairportrobotics/frc/robolib/vision/limelight/Limelight.java
@@ -4,14 +4,17 @@
 
 package org.fairportrobotics.frc.robolib.vision.limelight;
 import edu.wpi.first.math.estimator.SwerveDrivePoseEstimator3d;
+import edu.wpi.first.math.geometry.Pose3d;
 
 public class Limelight {
   private String name;
   private SwerveDrivePoseEstimator3d poseEstimator;
+  private Pose3d pose;
 
-  public Limelight(String name, SwerveDrivePoseEstimator3d poseEstimator) {
+  public Limelight(String name, SwerveDrivePoseEstimator3d poseEstimator, Pose3d pose) {
     this.name = name;
     this.poseEstimator = poseEstimator;
+    this.pose=pose;
   } 
 
   public LimelightHelpers.PoseEstimate updateOdometry() {      
